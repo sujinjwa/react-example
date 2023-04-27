9fdfc9946c7eb433cbec00662c56cfc9eca4265f
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-04-27 20:56:09 +0900

    init: Create React App으로 리액트 앱 생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let name = '수진';
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>안뇽 {name}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
