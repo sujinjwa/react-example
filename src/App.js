2d0b106e9c00049e3f1a8f28e101123d931c67ce
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-04-27 22:02:04 +0900

    feat: Counter 컴포넌트 만들고 데이터 상태 변화시키기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React from 'react';
+import Counter from './Counter';
 import './App.css';
 
 import MyHeader from './MyHeader';
 
 function App() {
-  let name = '수진';
-
-  const style = {
-    App: {
-      backgroundColor: 'black',
-    },
-    h2: {
-      color: 'yellow',
-    },
-  };
-
-  const func = () => {
-    return '함수 호출도 가능하군요';
-  };
-
-  let number = 5;
-
   return (
-    <div style={style.App} className='App'>
+    <div className='App'>
       <MyHeader />
-      <header className='App-header'>
-        <h2 style={style.h2}>안뇽 {name}</h2>
-        <p style={style.h2}>{1 + 2}</p>
-        <p style={style.h2}>{func()}</p>
-        <p style={style.h2}>{'하하하'}</p>
-      </header>
-      <b style={style.h2}>
-        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-      </b>
+      <Counter />
     </div>
   );
 }
