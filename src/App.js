949d705b00adc7c3654a0784b3f67d8c0a37222f
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-04-27 21:37:10 +0900

    feat: 컴포넌트 생성 및 JSX 활용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React from 'react';
 import './App.css';
+
+import MyHeader from './MyHeader';
 
 function App() {
   let name = '수진';
 
+  const style = {
+    App: {
+      backgroundColor: 'black',
+    },
+    h2: {
+      color: 'yellow',
+    },
+  };
+
+  const func = () => {
+    return '함수 호출도 가능하군요';
+  };
+
+  let number = 5;
+
   return (
-    <div className='App'>
+    <div style={style.App} className='App'>
+      <MyHeader />
       <header className='App-header'>
-        <p>안뇽 {name}</p>
+        <h2 style={style.h2}>안뇽 {name}</h2>
+        <p style={style.h2}>{1 + 2}</p>
+        <p style={style.h2}>{func()}</p>
+        <p style={style.h2}>{'하하하'}</p>
       </header>
+      <b style={style.h2}>
+        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
+      </b>
     </div>
   );
 }
